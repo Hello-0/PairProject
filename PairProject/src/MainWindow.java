5bb0727b59c1c9cff6857123f55bf0a5205b9015
Hello-0 <zhiw1999@gmail.com>
2017-04-05 09:42:56 -0400

    Update MainWindow.java

--- PairProject/src/MainWindow.java
+++ PairProject/src/MainWindow.java
@@ -1,4 +1,37 @@
-
+import java.util.*;
+import javax.swing.*;
 public class MainWindow {
-
+	JFrame frame;
+	Menu menu;
+	int width;
+	int height;
+	MainWindow() {
+		width = 1500;
+		height = 800;
+		
+		frame = new JFrame("Impossible Quiz");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		menu = new Menu(this);
+		frame.setContentPane(menu);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+	public void startQuiz(Quiz quiz) {
+		ArrayList<Question> questions = quiz.getQuestionList();
+		this.setContentArea(questions.get(0));
+	}
+	public void setContentArea(JPanel panel) {
+		frame.setContentPane(panel);
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public static void main(String[] args) {
+		new MainWindow();
+	}
 }
