bb10deb9e4921dbed6341bc4885b49a0c89f5d21
kdubbs0 <kwwong410@gmail.com>
2017-05-05 09:10:10 -0400

    lol shit. i'mma need you to do a lot of GUI shit

--- PairProject/src/Quiz.java
+++ PairProject/src/Quiz.java
@@ -5,45 +5,53 @@ public class Quiz {
 	MainWindow window;
 	Player player;
 	int questionNum;
-	JLabel life;
-	ArrayList<MultipleChoiceQuestion> mcQuestions;
-	ArrayList<Question> specQuestions, questions;
+	//JLabel life;
+	//ArrayList<MultipleChoiceQuestion> mcQuestions;
+	ArrayList<Question> questions;//specQuestions, ;
 	public Quiz(MainWindow window, Player player) {
 		this.window = window;
 		this.player = player;
 		questions = new ArrayList<Question>();
-		mcQuestions = new ArrayList<MultipleChoiceQuestion>();
-		specQuestions = new ArrayList<Question>();
+		//mcQuestions = new ArrayList<MultipleChoiceQuestion>();
+		//specQuestions = new ArrayList<Question>();
 		addQuestions();
 	}
 	private void addQuestions() {
 		MultipleChoiceQuestion mc1 = new MultipleChoiceQuestion(window, this, 1, "How many seconds in a year?", "31536000", "12", "60", "1", "b");
 		MultipleChoiceQuestion mc2 = new MultipleChoiceQuestion(window, this, 2, "I did, did I?", "No, I didn't", "Of course I did!", "He did, eh?", "Wait, what?", "c");
-		MultipleChoiceQuestion mc3 = new MultipleChoiceQuestion(window, this, 3, "How many letters in your name?",
+		SpecialQuestion1 sq1 = new SpecialQuestion1(window, this, 3);
+		MultipleChoiceQuestion mc3 = new MultipleChoiceQuestion(window, this, 4, "How many letters in your name?",
 				"8", Integer.toString(player.getName().replaceAll(" ", "").length()), "I forget my name", "Is this a trick question?", "a");
-		MultipleChoiceQuestion mc4 = new MultipleChoiceQuestion(window, this, 4, "See, oh double-you?", "What?", "Ouch", "Moo", "We are twins", "c");
-		MultipleChoiceQuestion mc5 = new MultipleChoiceQuestion(window, this, 5, "How many people in this class?", "11", "Do computers count?", "HOW MANY IN YOURS", "Depends on the class", "d");
-		MultipleChoiceQuestion mc6 = new MultipleChoiceQuestion(window, this, 6, "Ring ring ring", "Hello?", "Banana Phone", "STRANGER DANGER", "I don't even have a phone", "b");
-		
+		MultipleChoiceQuestion mc4 = new MultipleChoiceQuestion(window, this, 5, "See, oh double-you?", "What?", "Ouch", "Moo", "We are twins", "c");
+		MultipleChoiceQuestion mc5 = new MultipleChoiceQuestion(window, this, 6, "How many people in this class?", "11", "Do computers count?", "ZHI PUT IN HOW MANY IN YOURS", "It's just me", "d");
+		MultipleChoiceQuestion mc6 = new MultipleChoiceQuestion(window, this, 7, "Ring ring ring", "Hello?", "Banana Phone", "STRANGER DANGER", "I don't even have a phone", "b");
+		/**
 		mcQuestions.add(mc1);
 		mcQuestions.add(mc2);
 		mcQuestions.add(mc3);
 		mcQuestions.add(mc4);
 		mcQuestions.add(mc5);
 		mcQuestions.add(mc6);
-		
-		SpecialQuestion1 sq1 = new SpecialQuestion1(window, this, 7);
 		questions.add(sq1);
-			//add more when we make
-		
+		*/
 		questions = new ArrayList<Question>();
-		///**
+		questions.add(mc1);
+		questions.add(mc2);
+		questions.add(sq1);
+		questions.add(mc3);
+		questions.add(mc4);
+		questions.add(mc5);
+		questions.add(mc6);
+		/*
 		for (int i = 0; i < mcQuestions.size() + specQuestions.size(); i++)
 			if (mcQuestions.size() > i && mcQuestions.get(i).getQuestionNum() == i + 1)
 				questions.add(mcQuestions.get(i));
 			else if (specQuestions.size() > i && specQuestions.get(i).getQuestionNum() == i + 1)
 				questions.add(specQuestions.get(i));
-		//*/
+		
+		for (int i = 0; mcQuestions.size() + specQuestions.size() > 0; i++)
+			if (mcQuestions.size() > 0 && mcQuestions.get)
+		*/
 		/**
 			//this is for if we just don't use questionNum since it's bein a lil shit
 		boolean moreMC = true;
@@ -87,7 +95,6 @@ public class Quiz {
 		JPanel winPanel = new JPanel();
 		winPanel.setPreferredSize(new Dimension(window.getWidth(), window.getHeight()));
 		winPanel.setBackground(Color.BLUE);
-		
 		window.setContentPane(winPanel);
 	}
 	public void setLosePanel() {
--- PairProject/src/SpecialQuestion1.java
+++ PairProject/src/SpecialQuestion1.java
@@ -1,16 +1,47 @@
 import java.awt.*;
 import java.awt.event.*;
+
 import javax.swing.*;
 
-public class SpecialQuestion1 extends Question {
-	
+public class SpecialQuestion1 extends Question implements MouseMotionListener {
+	boolean start;
 	public SpecialQuestion1(MainWindow window, Quiz quiz, int questionNum) {
 		super(window, quiz, questionNum);
+		start = false;
 	}
 	public void createPanel() {
-		
+		//Paint panel = new Paint();
+	}/*
+	public void update(Graphics g) {
+		paintComponent(g);
+	}*/
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (!start) {
+			g.drawOval(window.getWidth() - 75, window.getHeight() / 2 - 75 / 2, 75, 75);
+		} else {
+			addMouseListener(new MouseListener() {
+				public void mouseClicked(MouseEvent e) {}
+				public void mouseEntered(MouseEvent e) {}
+				public void mouseExited(MouseEvent e) {
+					loseLife();
+				}
+				public void mousePressed(MouseEvent e) {}
+				public void mouseReleased(MouseEvent e) {}
+			});
+			addMouseMotionListener(new MouseAdapter() {
+				public void MouseMoved(MouseEvent e) {
+					
+				}
+			});
+			g.setColor(Color.GREEN);
+			g.fillRect(0, window.getHeight() / 2 - 75 / 2, 75, 75);
+			g.fillRect(window.getWidth() - 75, window.getHeight() / 2 - 75 / 2, 75, 75);
+		}
 	}
-
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
+	/*
 	class Paint extends JPanel {
 		 public void paintComponent(Graphics g) {
 				 super.paintComponent(g);
@@ -19,6 +50,7 @@ public class SpecialQuestion1 extends Question {
 		         g.fillRect(760,225,75,75);
 		 }
 	}
+	*/
 	class RightClickMenu extends JPopupMenu {
 	    JMenuItem anItem;
 	    public RightClickMenu(){
